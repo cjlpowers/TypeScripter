77d6e4b35826ff8787127e042542667f5b7a6459
Chris Powers <cpowers@outlook.com>
2015-04-04 21:05:24 -0500

    Updating tests

---
+++ TypeScripter.Tests/AssemblyTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+using TypeScripter.TypeScript;
+using TypeScripter.Readers;
+
+namespace TypeScripter.Tests
+{
+	[TestFixture]
+	public class AssemblyTest : TestBase
+	{
+		[Test]
+		public void SystemAssemblyTest()
+		{
+			var output = new StringBuilder();
+			output.Append(
+				new TypeScripter.Scripter()
+					.AddTypes(typeof(string).Assembly)
+			);
+			output.AppendLine();
+			output.AppendLine("var dateTime: System.DateTime;");
+			output.AppendLine("var result: System.DateTime = dateTime.AddDays(1);");
+
+			ValidateTypeScript(output);
+		}
+	}
+}
+
