65e6b75c9ec60ec81ef05c34b11e99e74168f809
Graham Ambrose <graham.ambrose@gmail.com>
2015-05-29 14:54:51 +0100

    added test for class with readonly fields

---
+++ TypeScripter.Tests/Fields.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using TypeScripter.Tests;
+
+namespace TypeScripter.Fields
+{
+    #region Example Constructs
+    public class Elephant
+    {
+        public readonly string Name;
+        public readonly int Age;
+
+        public Elephant(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+    }
+    #endregion
+
+    [TestFixture]
+    public class ReadonlyFieldsUsage : Test
+    {
+        [Test]
+        public void OutputTest()
+        {
+            var scripter = new TypeScripter.Scripter();
+            var output = scripter
+                .AddType(typeof(Elephant))
+                .ToString();
+
+            ValidateTypeScript(output);
+        }
+
+        [Test]
+        public void TestThatFieldIsRendered()
+        {
+            var scripter = new TypeScripter.Scripter();
+            var output = scripter
+                .AddType(typeof(Elephant))
+                .ToString();
+
+            Assert.True(output.Contains("Name"));
+        }
+    }
+
+}
+
