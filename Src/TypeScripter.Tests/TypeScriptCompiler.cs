4105572526490e4a0f0963359e4c95cf185843cf
Chris Powers <cpowers@outlook.com>
2018-02-25 02:06:07 -0600

    Target .NET Standard 1.5

--- Src/TypeScripter.Tests/TypeScriptCompiler.cs
+++ Src/TypeScripter.Tests/TypeScriptCompiler.cs
@@ -68,11 +68,7 @@ namespace TypeScripter.Tests
         {
             var options = "";
             var process = new Process();
-#if DOTNET
-            process.StartInfo.FileName = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), @"..\..\", @"Tools\Microsoft.TypeScript.Compiler\tsc.exe");
-#else
             process.StartInfo.FileName = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), @"..\..\..\..\..\", @"Tools\Microsoft.TypeScript.Compiler\tsc.exe");
-#endif
             process.StartInfo.Arguments = string.Format("{0} {1}", options, string.Join(" ", files));
             process.StartInfo.CreateNoWindow = true;
             process.StartInfo.UseShellExecute = false;
